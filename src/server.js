f2c530b46b85649dbe48370b376d495f835a64c2
taekyun-yoon <tk.yoon0105@gmail.com>
2024-03-21 04:16:24 +0900

    feat: add login&signup page

--- src/server.js
+++ src/server.js
@@ -10,10 +10,29 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 mongoose.connect(`mongodb+srv://paul:wizen5128@express-auth-project.qvfovwd.mongodb.net/?retryWrites=true&w=majority&appName=express-auth-project`)
     .then(() => console.log('mongodb connected'))
     .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}...`);
+});
+
+
+app.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+app.post('/login', function(req, res, next) {
+});
+
+app.get('/signup', function(req, res, next) {
+    res.render('signup');
+});
+
+app.post('/signup', function(req, res, next) {
 });

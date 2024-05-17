5f066b20c860e92d54531ba1429e23ffdca4c94d
taekyun-yoon <tk.yoon0105@gmail.com>
2024-05-17 21:30:38 +0900

    feat: post 좋아요 ajax 처리

--- src/routes/likes.router.js
+++ src/routes/likes.router.js
@@ -1,46 +1,29 @@
 const express = require('express');
 const likesRouter = express.Router({
     mergeParams: true
-    });
+});
 const { checkAuthenticated } = require('../middleware/auth');
 const Post = require('../models/posts.model');
 
 likesRouter.put('/', checkAuthenticated, async (req, res) => {
-    try{
+    try {
         const post = await Post.findById(req.params.id);
-        console.log('포스트: ', post);
-
-        if(post.likes.find(like => like === req.user._id.toString())) {
-            const updatedLikes = post.likes.filter(like => like !== req.user._id.toString());
-            try{
-                await Post.findByIdAndUpdate(post._id, {likes: updatedLikes});
-
-                req.flash('success', '게시글 좋아요 업데이트 완료.');
-                res.redirect('/posts');
-            }catch (err) {
-                console.error(err);
-                req.flash('error', '게시글 좋아요 업데이트 중에 에러가 발생하였습니다.');
-                res.redirect('/posts');
-            }
-        }else{
-            try{
-                await Post.findByIdAndUpdate(post._id, {
-                    likes: post.likes.concat([req.user._id])});
-                req.flash('success', '게시글 좋아요 업데이트 완료.');
-                res.redirect('/posts');
-        }catch(err) {
-            console.error(err);
-            req.flash('error', '게시글 좋아요 업데이트 중에 에러가 발생하였습니다.');
-            res.redirect('/posts');
+        let updatedLikes;
+        if (post.likes.includes(req.user._id.toString())) {
+            // 좋아요 취소 로직
+            updatedLikes = post.likes.filter(like => like !== req.user._id.toString());
+        } else {
+            // 좋아요 추가 로직
+            updatedLikes = post.likes.concat([req.user._id.toString()]);
         }
 
-        }
-    }catch(err) {
+        await Post.findByIdAndUpdate(post._id, { likes: updatedLikes });
+
+        res.json({ success: true, likesCount: updatedLikes.length, liked: !post.likes.includes(req.user._id.toString()) });
+    } catch (err) {
         console.error(err);
-        req.flash('error', '좋아요 한 게시글이 없거나 에러가 발생하였습니다.');
-        res.redirect('/posts');
+        res.status(500).json({ success: false, message: '좋아요 업데이트 중에 에러가 발생하였습니다.' });
     }
-})
-
+});
 
 module.exports = likesRouter;
